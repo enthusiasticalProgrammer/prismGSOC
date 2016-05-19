c0e6c9c51c5f830903718d4c193a05d27db5db13
ga25suc <ga25suc@mytum.de>
2016-05-19 12:16:12 +0200

    consolidated merge in ModelType

--- src/prism/ModelType.java
+++ src/prism/ModelType.java
@@ -148,7 +148,6 @@ public enum ModelType
 	 * Do the transitions in a choice sum to 1 for this model type?
 	 * Can also use this to test whether models uses rates or probabilities.
 	 */
-	//TODO Christpher: does this make sense
 	public boolean choicesSumToOne()
 	{
 		return true;
@@ -165,7 +164,6 @@ public enum ModelType
 	/**
 	 * Does this model allow nondeterministic choices?
 	 */
-	//TODO Christopher: does this make sense?
 	public boolean nondeterministic()
 	{
 		return true;
