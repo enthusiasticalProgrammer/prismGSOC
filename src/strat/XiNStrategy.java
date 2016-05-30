fd8c1046556d5e80b65c8dc99be6d8aafc6108e6
ga25suc <ga25suc@mytum.de>
2016-05-30 09:33:36 +0200

    fixed name error (used wrong greek letter)

--- src/strat/XiNStrategy.java
+++ src/strat/XiNStrategy.java
@@ -8,36 +8,36 @@ import prism.PrismException;
 import prism.PrismLog;
 
 //TODO @Christopher: add Documentation
-public class zetaNStrategy implements Strategy
+public class XiNStrategy implements Strategy
 {
 	BigInteger countSteps;
-	
+
 	/**Corresponds to j in paper*/
 	int phase;
-	
+
 	BigInteger stepsUntilNextPhase;
-	int state; 
+	int state;
 
 	@Override
 	public void init(int state) throws InvalidStrategyStateException
 	{
-		this.state=state;
-		this.phase=0;
-		this.countSteps=BigInteger.valueOf(0);
-		this.stepsUntilNextPhase=n(0);
+		this.state = state;
+		this.phase = 0;
+		this.countSteps = BigInteger.valueOf(0);
+		this.stepsUntilNextPhase = n(0);
 	}
 
 	@Override
 	public void updateMemory(int action, int state) throws InvalidStrategyStateException
 	{
-		this.state=state;
-		if(stepsUntilNextPhase.equals(BigInteger.ZERO)){
+		this.state = state;
+		if (stepsUntilNextPhase.equals(BigInteger.ZERO)) {
 			phase++;
-			stepsUntilNextPhase=n(phase);
-		}else{
-			stepsUntilNextPhase=stepsUntilNextPhase.subtract(BigInteger.ONE);
+			stepsUntilNextPhase = n(phase);
+		} else {
+			stepsUntilNextPhase = stepsUntilNextPhase.subtract(BigInteger.ONE);
 		}
-		countSteps=countSteps.add(BigInteger.ONE);
+		countSteps = countSteps.add(BigInteger.ONE);
 	}
 
 	@Override
@@ -51,14 +51,14 @@ public class zetaNStrategy implements Strategy
 	public void reset()
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exportToFile(String file)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -79,7 +79,7 @@ public class zetaNStrategy implements Strategy
 	public void setInfo(String info)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -107,7 +107,7 @@ public class zetaNStrategy implements Strategy
 	public void setMemory(Object memory) throws InvalidStrategyStateException
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -128,42 +128,42 @@ public class zetaNStrategy implements Strategy
 	public void exportActions(PrismLog out)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exportIndices(PrismLog out)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exportInducedModel(PrismLog out)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exportDotFile(PrismLog out)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initialise(int s)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Object action, int s)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -177,40 +177,43 @@ public class zetaNStrategy implements Strategy
 	public void clear()
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	/**
 	 * returns n_j from paper TODO: cite
 	 * TODO: almost instantly overflows --> fix with Math.BigInt 
 	 */
-	private BigInteger n(long j){
-		if(j<0)
+	private BigInteger n(long j)
+	{
+		if (j < 0)
 			throw new IllegalArgumentException();
-		if(j==0){
-			return kappa(j+1);
+		if (j == 0) {
+			return kappa(j + 1);
 		}
-		BigInteger result= kappa(j+1).min( n(j-1)).multiply(BigInteger.valueOf(2L));
-		
-		for(long i=0;i<j;i++){
-			result=result.multiply(BigInteger.valueOf(2L));
+		BigInteger result = kappa(j + 1).min(n(j - 1)).multiply(BigInteger.valueOf(2L));
+
+		for (long i = 0; i < j; i++) {
+			result = result.multiply(BigInteger.valueOf(2L));
 		}
 		return result;
 	}
-	
+
 	/**
 	 * TODO howto compute it
 	 */
-	private BigInteger kappa(long j){
-		if(j<0)
+	private BigInteger kappa(long j)
+	{
+		if (j < 0)
 			throw new IllegalArgumentException();
 		return BigInteger.valueOf(10L);
 	}
-	
+
 	/**
 	 * TODO howto compute it
 	 * */
-	private double M(){
-		return phase*10.0;
+	private double M()
+	{
+		return phase * 10.0;
 	}
 }
