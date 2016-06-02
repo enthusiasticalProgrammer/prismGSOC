db64595d287f4e297162ef3c90872328a9d6bc03
ga25suc <ga25suc@mytum.de>
2016-06-02 17:25:31 +0200

    extended Distribution

--- test/strat/TestMultiLongRunStrategy.java
+++ test/strat/TestMultiLongRunStrategy.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import explicit.Distribution;
 import explicit.MultiLongRun;
 import explicit.TestMultiLongRun;
 import prism.PrismException;
