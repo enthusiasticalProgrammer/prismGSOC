c2f433fc9b42d367ae1aeecf46b37cf92cf897c3
ga25suc <ga25suc@mytum.de>
2016-07-19 09:50:58 +0200

    fixed bug, which exists due to merging

--- prism/src/parser/visitor/CheckValid.java
+++ prism/src/parser/visitor/CheckValid.java
@@ -55,12 +55,6 @@ public class CheckValid extends ASTTraverse
 		}
 		// PTA only support upper time bounds
 		if (e.getLowerBound() != null) {
-			if (modelType == ModelType.DTMC) {
-				throw new PrismLangException("Only upper time bounds are allowed on the " + e.getOperatorSymbol() + " operator for DTMCs");
-			}
-			if (modelType == ModelType.MDP) {
-				throw new PrismLangException("Only upper time bounds are allowed on the " + e.getOperatorSymbol() + " operator for MDPs");
-			}
 			if (modelType == ModelType.PTA) {
 				throw new PrismLangException("Only upper time bounds are allowed on the " + e.getOperatorSymbol() + " operator for PTAs");
 			}
