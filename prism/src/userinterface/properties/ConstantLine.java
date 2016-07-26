f8114abf9be7aa0e6caa148b9a82db501094fa50
ga25suc <ga25suc@mytum.de>
2016-07-26 15:18:57 +0200

    added type parameter in ConstantLine

--- prism/src/userinterface/properties/ConstantLine.java
+++ prism/src/userinterface/properties/ConstantLine.java
@@ -285,7 +285,7 @@ public class ConstantLine extends javax.swing.JPanel
 		java.awt.GridBagConstraints gridBagConstraints;
 
 		choiceButtonGroup = new javax.swing.ButtonGroup();
-		boolSingleValueCombo = new javax.swing.JComboBox();
+		boolSingleValueCombo = new javax.swing.JComboBox<>();
 		sizerPanel = new javax.swing.JPanel();
 		nameLabel = new javax.swing.JLabel();
 		typeLabel = new javax.swing.JLabel();
@@ -409,7 +409,7 @@ public class ConstantLine extends javax.swing.JPanel
 	}//GEN-LAST:event_singleValueComboActionPerformed
 
 	// Variables declaration - do not modify//GEN-BEGIN:variables
-	javax.swing.JComboBox boolSingleValueCombo;
+	javax.swing.JComboBox<String> boolSingleValueCombo;
 	private javax.swing.ButtonGroup choiceButtonGroup;
 	javax.swing.JTextField endValueField;
 	javax.swing.JLabel nameLabel;
