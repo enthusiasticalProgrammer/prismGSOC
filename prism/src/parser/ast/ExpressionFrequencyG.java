72646412f7f1ffa84e9739b9ebb9b3a94fcfa55a
ga25suc <ga25suc@mytum.de>
2016-07-27 16:04:55 +0200

    added ExpressionFrequencyG

---
+++ prism/src/parser/ast/ExpressionFrequencyG.java
@@ -0,0 +1,27 @@
+package parser.ast;
+
+import ltl.parser.Comparison;
+
+public class ExpressionFrequencyG extends ExpressionTemporal
+{
+	public final double bound;
+	public final Comparison cmpOperator; // the cmpOperator
+	public final boolean isLimInf; //true if we are considering lim inf, else: false
+
+	public ExpressionFrequencyG(double bound, Comparison cmpOperator, boolean isLimInf)
+	{
+		this.bound = bound;
+		this.cmpOperator = cmpOperator;
+		this.isLimInf = isLimInf;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+		ExpressionFrequencyG freq = (ExpressionFrequencyG) o;
+		return super.equals(o) && this.bound == freq.bound && this.cmpOperator == freq.cmpOperator && this.isLimInf == freq.isLimInf;
+	}
+}
